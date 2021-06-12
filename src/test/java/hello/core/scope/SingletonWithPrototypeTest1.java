ae3911489fac5d26ed27d2e46d4b4d604cca660f
kid7258 <kid7258@naver.com>
2021-06-12 12:14:35 +0900

    프로토타입 스코프 - javax provider 사용

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 public class SingletonWithPrototypeTest1 {
 
@@ -40,10 +41,10 @@ public class SingletonWithPrototypeTest1 {
     @Scope("singleton")
     static class ClientBean {
         @Autowired
-        private ObjectProvider<PrototypeBean> prototypeBeansProvider;
+        private Provider<PrototypeBean> prototypeBeansProvider;
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeansProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeansProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
