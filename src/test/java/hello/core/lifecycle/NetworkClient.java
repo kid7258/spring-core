19bd7271720b3523787c4c4035dc6895e2c39c8b
kid7258 <kid7258@naver.com>
2021-06-12 11:58:05 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용 시, ObjectProvider로 해결

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -27,12 +30,14 @@ public class NetworkClient {
     }
 
     // 의존 관계 주입이 끝나면 호출 됨
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
